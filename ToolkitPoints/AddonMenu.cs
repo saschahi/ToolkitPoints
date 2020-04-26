a78a246a84128aeadd43d60858a9ab95c0cbb23e
hodlhodl <don@dmsgmail.com>
2020-04-26 02:14:13 -0700

    init

---
+++ ToolkitPoints/AddonMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolkitCore.Interfaces;
+using ToolkitCore.Windows;
+using Verse;
+
+namespace ToolkitPoints
+{
+    public class AddonMenu : IAddonMenu
+    {
+        List<FloatMenuOption> IAddonMenu.MenuOptions() => new List<FloatMenuOption>
+        {
+            new FloatMenuOption("Settings", delegate ()
+            {
+                Window_ModSettings window = new Window_ModSettings(LoadedModManager.GetMod<ToolkitPoints>());
+                Find.WindowStack.TryRemove(window.GetType());
+                Find.WindowStack.Add(window);
+            })
+        };
+    }
+}
---
+++ ToolkitPoints/ToolkitPoints.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ToolkitPoints
+{
+    public class ToolkitPoints : Mod
+    {
+        public ToolkitPoints(ModContentPack content) : base(content)
+        {
+
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            ToolkitPointsSettings.DoSettingsWindowContents(inRect);
+        }
+    }
+}
