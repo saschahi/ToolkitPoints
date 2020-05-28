290ed97fe97a24257fdab590eea6cfadc3490cc7
hodlhodl <don@dmsgmail.com>
2020-05-28 11:38:18 -0700

    feat: Commands to Award and Take points from viewers

--- ToolkitPoints/CommandMethods/AwardPoints.cs
+++ ToolkitPoints/CommandMethods/AwardPoints.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolkitCore;
 using ToolkitCore.Models;
 using TwitchLib.Client.Interfaces;
+using Verse;
 
 namespace ToolkitPoints.CommandMethods
 {
@@ -17,7 +19,25 @@ namespace ToolkitPoints.CommandMethods
 
         public override void Execute(ITwitchCommand twitchCommand)
         {
-            twitchCommand. 
+            List<string> args = twitchCommand.ArgumentsAsList; 
+
+            if (args.Count < 2)
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} to award {ToolkitPointsSettings.pointsBaseName} make sure to include a username then an amount.");
+                return;
+            }
+
+            string username = args[0].Replace("@", "");
+
+            if (!int.TryParse(args[1], out int points))
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} to award {ToolkitPointsSettings.pointsBaseName} make sure to include the amount after the username.");
+                return;
+            }
+
+            Points.AddPoints(username, points);
+
+            TwitchWrapper.SendChatMessage($"@{username} you have been awarded {points} {ToolkitPointsSettings.pointsBaseName} from @{twitchCommand.Username}");
         }
     }
 }
---
+++ ToolkitPoints/CommandMethods/TakePoints.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolkitCore;
+using ToolkitCore.Models;
+using TwitchLib.Client.Interfaces;
+
+namespace ToolkitPoints.CommandMethods
+{
+    public class TakePoints : CommandMethod
+    {
+        public TakePoints(ToolkitChatCommand command) : base(command)
+        {
+
+        }
+
+        public override void Execute(ITwitchCommand twitchCommand)
+        {
+            List<string> args = twitchCommand.ArgumentsAsList;
+
+            if (args.Count < 2)
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} to take {ToolkitPointsSettings.pointsBaseName} make sure to include a username then an amount.");
+                return;
+            }
+
+            string username = args[0].Replace("@", "");
+
+            if (!int.TryParse(args[1], out int points))
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} to take {ToolkitPointsSettings.pointsBaseName} make sure to include the amount after the username.");
+                return;
+            }
+
+            Points.RemovePoints(username, points);
+
+            TwitchWrapper.SendChatMessage($"@{username} you have had {points} {ToolkitPointsSettings.pointsBaseName} taken from you by @{twitchCommand.Username}");
+        }
+    }
+}
